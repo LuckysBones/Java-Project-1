3e4e3fa3b33eb140976199c3c6bb79d4732d9cd2
Luc Gremillion <LuckysBones@gmail.com>
2024-03-04 23:53:18 -0700

    First Commit

---
+++ src/edu/frontrange/csc240/exam1/ExamOneTest.java
@@ -0,0 +1,11 @@
+package edu.frontrange.csc240.exam1;
+
+public class ExamOneTest
+{
+public static void main(String args[])
+{
+	AccountTester testOne = new AccountTester();
+	testOne.testAccountClass();  
+                
+}
+}
